1f7a57115b49823e25dd2c593013d1b197a9a50e
mimjs <mikhailmateevskiy@gmail.com>
2021-12-29 01:18:39 +0300

    main

--- src/views/home/games.js
+++ src/views/home/games.js
@@ -11,15 +11,15 @@ const Games = ({ id, openGame }) => {
         </PanelHeaderContent>
       </PanelHeader>
       <div className="panel--in game--list">
-        <Game gameName="Double dice" onlineCount={1} className="game--doubleDice"/>
+        <Game gameName="Double dice" onlineCount={489} className="game--doubleDice"/>
         <Game gameName="Dice" onlineCount={96} className="game--dice"/>
-        <Game gameName="Dream Catcher" onlineCount={1} className="game--dreamCatcher"/>
-        <Game gameName="Blackjack" onlineCount={1} className="game--blackjack"/>
-        <Game gameName="Tower" onlineCount={1} className="game--tower"/>
-        <Game gameName="Keno" onlineCount={1} className="game--keno"/>
-        <Game gameName="Под 7 над" onlineCount={1} className="game--b7m"/>
-        <Game gameName="Dicewheel" onlineCount={1} className="game--dicewheel"/> 
-        <Game gameName="Thimble" onlineCount={1} className="game--thimble"/>
+        <Game gameName="Dream Catcher" onlineCount={2} className="game--dreamCatcher"/>
+        <Game gameName="Blackjack" onlineCount={0} className="game--blackjack"/>
+        <Game gameName="Tower" onlineCount={5} className="game--tower"/>
+        <Game gameName="Keno" onlineCount={0} className="game--keno"/>
+        <Game gameName="Под 7 над" onlineCount={69} className="game--b7m"/>
+        <Game gameName="Dicewheel" onlineCount={234} className="game--dicewheel"/> 
+        <Game gameName="Thimble" onlineCount={0} className="game--thimble"/>
       </div>
     </Panel>
   );
