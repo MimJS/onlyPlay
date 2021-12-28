5f308c2e716ec3edee47a932a10550f7494153ce
evgeny-k-dev <trapetsio@yandex.ru>
2021-12-29 01:36:53 +0300

    Button removed active effect

--- src/views/home/games.js
+++ src/views/home/games.js
@@ -10,16 +10,16 @@ const Games = ({ id, openGame }) => {
           Игры
         </PanelHeaderContent>
       </PanelHeader>
-      <div className="panel--in game--l ist">
-        <Game gameName="Double dice" onlineCount={1} className="game--doubleDice"/>
+      <div className="panel--in game--list">
+        <Game gameName="Double dice" onlineCount={0} className="game--doubleDice"/>
         <Game gameName="Dice" onlineCount={96} className="game--dice"/>
         <Game gameName="Dream Catcher" onlineCount={1} className="game--dreamCatcher"/>
-        <Game gameName="Blackjack" onlineCount={1} className="game--blackjack"/>
-        <Game gameName="Tower" onlineCount={1} className="game--tower"/>
-        <Game gameName="Keno" onlineCount={1} className="game--keno"/>
-        <Game gameName="Под 7 над" onlineCount={1} className="game--b7m"/>
-        <Game gameName="Dicewheel" onlineCount={1} className="game--dicewheel"/> 
-        <Game gameName="Thimble" onlineCount={1} className="game--thimble"/>
+        <Game gameName="Blackjack" onlineCount={0} className="game--blackjack"/>
+        <Game gameName="Tower" onlineCount={0} className="game--tower"/>
+        <Game gameName="Keno" onlineCount={11} className="game--keno"/>
+        <Game gameName="Под 7 над" onlineCount={21} className="game--b7m"/>
+        <Game gameName="Dicewheel" onlineCount={13} className="game--dicewheel"/> 
+        <Game gameName="Thimble" onlineCount={41} className="game--thimble"/>
       </div>
     </Panel>
   );
