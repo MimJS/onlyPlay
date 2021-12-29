345b811beb1dd6845cd77e066903ac52ae821f52
evgeny-k-dev <trapetsio@yandex.ru>
2021-12-29 06:28:13 +0300

    Create format.js

---
+++ src/lib/format.js
@@ -0,0 +1,12 @@
+export default function formatNumber(number) {
+    let k = "";
+    while (number >=1000){
+        number/=1000;
+        k+="K";
+    }
+    const n = number.toLocaleString('en-US', {
+        useGrouping: false,
+        maximumFractionDigits: 2
+    }).split('.');
+    return [n[0],'.',n[1],k].join('')
+}
